b86c15c4b968e4fd069225efd7cb0a5fa6e13677
cebilon123 <michalkowal567@gmail.com>
2022-07-21 17:36:12 +0200

    Clean old project

--- main.go
+++ main.go
@@ -1,26 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/cebilon123/KE1pY2hhxYJfIkdvZ29BcHBzIE5BU0EiKQ-/app"
-	"github.com/cebilon123/KE1pY2hhxYJfIkdvZ29BcHBzIE5BU0EiKQ-/config"
-	"github.com/cebilon123/KE1pY2hhxYJfIkdvZ29BcHBzIE5BU0EiKQ-/infrastructure"
-	"github.com/cebilon123/KE1pY2hhxYJfIkdvZ29BcHBzIE5BU0EiKQ-/server"
-	"net/http"
-	_ "net/http/pprof"
-)
-
-//""
 func main() {
-	sConfig := config.NewStartup()
 
-	if err := server.NewServer().
-		WithPort(fmt.Sprintf(":%s", sConfig.Port)).
-		WithConfig(sConfig.ApiKey, int8(sConfig.MaxConcurrentNasaRequests)).
-		AddHandler("/pictures", func(w http.ResponseWriter, r *http.Request) {
-			app.NewNasaPicturesHandler().Handle(w, r, infrastructure.NewNasaImageProvider())
-		}).
-		Start(); err != nil {
-		panic(err)
-	}
 }
